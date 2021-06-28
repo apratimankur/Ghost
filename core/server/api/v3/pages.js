0ccf37f11c694eed79ee8ef472cba4ffdceb8432
Apratim <apratim.ankur@gmail.com>
2021-06-29 01:05:35 +0530

    pages schema fix, en.json minor updates, reset-password mail template minor update

--- core/server/api/v3/pages.js
+++ core/server/api/v3/pages.js
@@ -2,7 +2,7 @@ const models = require('../../models');
 const i18n = require('../../../shared/i18n');
 const errors = require('@tryghost/errors');
 const urlUtils = require('../../../shared/url-utils');
-const ALLOWED_INCLUDES = ['tags', 'authors', 'authors.roles'];
+const ALLOWED_INCLUDES = ['tags', 'authors', 'authors.roles', 'label', 'taxon', 'usedIn'];
 const UNSAFE_ATTRS = ['status', 'authors', 'visibility'];
 
 module.exports = {
